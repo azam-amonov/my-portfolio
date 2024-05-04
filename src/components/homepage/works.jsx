2fb7379db521aa1cdc3a0041bad0625672aa6c4e
Azam Amonov <azamamonov555@gmail.com>
2024-05-04 16:33:13 +0500

    CODE RUB: Add Work Experience

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -10,33 +10,33 @@ const Works = () => {
 		<div className="works">
 			<Card
 				icon={faBriefcase}
-				title="Work"
+				title="Work Experiance"
 				body={
 					<div className="works-body">
 						<div className="work">
 							<img
-								src="./facebook.png"
+								src="./tarteeb.jpg"
 								alt="facebook"
 								className="work-image"
 							/>
-							<div className="work-title">Facebook</div>
+							<div className="work-title">Tarteeb</div>
 							<div className="work-subtitle">
-								Software Engineer
+								.NET Developer (Intership)
 							</div>
-							<div className="work-duration">2019 - Present</div>
+							<div className="work-duration">2024/01 - Present </div>
 						</div>
 
 						<div className="work">
 							<img
-								src="./twitter.png"
+								src="./najotlogo.png"
 								alt="twitter"
 								className="work-image"
 							/>
-							<div className="work-title">Twitter</div>
+							<div className="work-title">Najot Ta'lim</div>
 							<div className="work-subtitle">
-								Software Engineer
+								Foundation Mentor & Principal Examiner
 							</div>
-							<div className="work-duration">2019 - Present</div>
+							<div className="work-duration">2023/03 - 2024/04</div>
 						</div>
 					</div>
 				}
